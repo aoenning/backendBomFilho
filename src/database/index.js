6653da4c86c57d42d002d40aad16984dbb628b62
aoenning <and.oenning@gmail.com>
2021-10-04 10:26:22 -0400

    Configuração sequelize

--- src/database/index.js
+++ src/database/index.js
@@ -1,23 +1,18 @@
 import Sequelize from "sequelize";
-import databaseConfig from "../config/database";
-import User from '../app/model/User';
-import Produtos from '../app/model/Produtos';
-import Vendedores from '../app/model/Vendedores';
-import Clientes from '../app/model/Clientes';
+
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false, // very important
+//         }
+//     }
+// });
+
+// sequelize
+//     .authenticate()
+//     .then((s) => { 'conectado com sucesso' })
+//     .catch((err) => { err })
 
 
-const models = [User, Clientes, Produtos, Vendedores];
-
-class Database {
-    constructor() {
-        this.init();
-    }
-
-    init() {
-        // this.connection = new Sequelize(databaseConfig);
-        this.connection = new Sequelize(process.env.DATABASE_URL);
-        models.map(model => model.init(this.connection));
-    }
-}
-
-export default new Database();
+// module.exports = sequelize;
