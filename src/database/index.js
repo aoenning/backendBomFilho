8fb8e45cb44d8c7946e8c97d813c19f08328ccbe
aoenning <and.oenning@gmail.com>
2021-10-04 10:37:40 -0400

    Configuração sequelize

--- src/database/index.js
+++ src/database/index.js
@@ -1,18 +1,18 @@
 import Sequelize from "sequelize";
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//     dialectOptions: {
-//         ssl: {
-//             require: true,
-//             rejectUnauthorized: false, // very important
-//         }
-//     }
-// });
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // very important
+        }
+    }
+});
 
-// sequelize
-//     .authenticate()
-//     .then((s) => { 'conectado com sucesso' })
-//     .catch((err) => { err })
+sequelize
+    .authenticate()
+    .then((s) => { 'conectado com sucesso' })
+    .catch((err) => { err })
 
 
-// module.exports = sequelize;
+module.exports = sequelize;
