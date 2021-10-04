74776fcfbc29a6075fc33bd745bed784af214216
aoenning <and.oenning@gmail.com>
2021-10-04 09:05:04 -0400

    sexto commit

--- src/database/index.js
+++ src/database/index.js
@@ -14,8 +14,8 @@ class Database {
     }
 
     init() {
-        this.connection = new Sequelize(databaseConfig);
-        // this.connection = new Sequelize(process.env.DATABASE_URL);
+        // this.connection = new Sequelize(databaseConfig);
+        this.connection = new Sequelize(process.env.DATABASE_URL);
         models.map(model => model.init(this.connection));
     }
 }
