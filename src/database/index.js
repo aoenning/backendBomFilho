ba66bc1aadeb33b49b7c7e6e4f3d59d60ebb38af
aoenning <and.oenning@gmail.com>
2021-10-04 08:58:26 -0400

    Quinto commit

--- src/database/index.js
+++ src/database/index.js
@@ -15,6 +15,7 @@ class Database {
 
     init() {
         this.connection = new Sequelize(databaseConfig);
+        // this.connection = new Sequelize(process.env.DATABASE_URL);
         models.map(model => model.init(this.connection));
     }
 }
--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
 import dotenv from 'dotenv';
 import app from './app';
-
-
 dotenv.config();
 
-
-app.listen(process.env.PORT || 3333, () => {
-    console.log('Servidor rodando na porta 33333')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('Servidor rodando')
 });
